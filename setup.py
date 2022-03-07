b636a1528782c056866e38505261401a4e86d463
Maryam Shahid <maryamshahid673@gmail.com>
2022-03-08 00:18:57 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup
 
 setup(
-    # TODO: Write a globally unique name which will be listed on PyPI
-    name="very-simple-dictionary",
-    author="Emre Sülün",  # TODO: Write your name
+    name="maryam-shahid-dictionary",
+    author="Maryam Shahid",
     version="2.0.0",
     packages=["dictionary"],
     install_requires=[
         "requests>=2.23.0",
     ],
     python_requires=">=3.8",
-
 )
