232d9e6e1efde1797d250169f444621c73ed1e0d
Maryam Shahid <maryamshahid673@gmail.com>
2022-03-09 20:12:39 +0300

    incremented version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="maryam-shahid-dictionary",
     author="Maryam Shahid",
-    version="2.0.0",
+    version="2.0.1",
     packages=["dictionary"],
     install_requires=[
         "requests>=2.23.0",
